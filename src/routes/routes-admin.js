066c4e54786fe2fefe316a81fed88ae6985ed03a
Rodrigo García <hola@rodrigogarcia.com.mx>
2019-07-28 14:02:57 -0500

    added default admin style and html

--- src/routes/routes-admin.js
+++ src/routes/routes-admin.js
@@ -7,16 +7,22 @@ const { config } = require('../config/config-app'),
 const adminModel = require('../models/model-admin'),
     userModel = require('../models/model-user');
 
+//-- Vista login
 router.get('/administrador/login', (req, res) => {
     res.render('admin/login', {
-        title: 'Registro de Administrador'
+        title: 'Login de Administrador'
     });
 });
-
+//-- Vista registro
 router.get('/administrador/registro', (req, res) => {
     res.render('admin/register', {
         title: 'Registro de Administrador'
     });
 });
-
+//-- Vista dashboard
+router.get('/administrador', (req, res) => {
+    res.render('admin/dashboard', {
+        title: 'Dashboard'
+    });
+});
 module.exports = router;
